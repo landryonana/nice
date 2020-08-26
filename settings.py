0da6f424c392188341830c478b7f792efd13d5ad
landryonana <51939280+landryonana@users.noreply.github.com>
2020-08-26 10:29:22 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
